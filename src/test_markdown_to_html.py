02bf3ab0c873d8f1a256d6136635144a78bd0e89
LS-2ednar <lukas_schaub@gmx.ch>
2025-08-16 12:03:42 +0200

    wrote some additonal tests for markdown_to_html

--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -4,7 +4,7 @@ from htmlnode import *
 from utils import *
 
 class Test_Markdown_to_html(unittest.TestCase):
-    def test_paragraphs(self):
+    def test_paragraphs_1(self):
         md = """
 This is **bolded** paragraph
 text in a p
@@ -16,3 +16,17 @@ This is another paragraph with _italic_ text and `code` here
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(html,"<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",)
+
+    def test_paragraphs_2(self):
+        md = """
+This is _italic_ paragraph
+_also this text is italic_ in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html,"<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",)
+
