bc79f1dc730cd6b8cba5d7054f0a5e948000d924
LS-2ednar <lukas_schaub@gmx.ch>
2025-08-18 16:58:04 +0200

    additonal test added

--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -30,3 +30,15 @@ This is another paragraph with _italic_ text and `code` here
         html = node.to_html()
         self.assertEqual(html,"<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",)
 
+        def test_paragraphs_3(self):
+        md = """
+This is _italic_ paragraph
+_also this text is italic_ in a p
+tag here. Did we mention this **bold** stuff here?
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html,"<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here. Did we mention this <b>bold</b> stuff here?</p></div>",)
