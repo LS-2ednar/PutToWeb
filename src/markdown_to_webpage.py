acde195a45a3dd52dfbe7d00d33fbf12fa57fcfc
LS-2ednar <lukas_schaub@gmx.ch>
2025-08-24 22:18:50 +0200

    more fixes

--- src/markdown_to_webpage.py
+++ src/markdown_to_webpage.py
@@ -24,8 +24,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
     html_content = markdown_to_html_node(page_content).to_html()
     title = extract_title(page_content)
-    full_html = template.replace("{{ Title }}",title).replace("{{ Content }}",html_content)
-    """full_html = full_html.replace('href="/',f'href="/{basepath}').replace('src="/',f'src="{basepath}')"""
+    full_html = template.replace("{{ Title }}",title).replace("{{ Content }}",html_content).replace('href="/',f'href="{basepath}').replace('src="/',f'src="{basepath}')
 
     with open(dest_path,"w+") as file:
         file.write(full_html)
@@ -42,13 +41,12 @@ def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, b
     for content in os.listdir(dir_path_content):
         content_path = os.path.join(dir_path_content,content)
         dest_path = os.path.join(dest_dir_path,content.replace(".md",".html"))
-        new_basepath = os.path.join(basepath,content)
 
         if content.endswith(".md"):
             print("creating html file for:")
             print(f"{dir_path_content}/{content}")
-            generate_page(content_path,template_path,dest_path, new_basepath)
+            generate_page(content_path,template_path,dest_path, basepath)
         else:
             if not os.path.isfile(content_path) and not os.path.exists(dest_path):
                 os.mkdir(dest_path)
-                generate_pages_recursively(content_path, template_path, dest_path, new_basepath)
+                generate_pages_recursively(content_path, template_path, dest_path, basepath)
