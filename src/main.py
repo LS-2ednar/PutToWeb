48170e616996ea9636757feb1a31cc1bef8dcb00
LS-2ednar <lukas_schaub@gmx.ch>
2025-08-24 10:00:35 +0200

    css hotfix

--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ import sys
 def main():
     if len(sys.argv) != 2:
         basepath = "/"
+        copy_to = "public"
     else:
         basepath = sys.argv[1]
+        copy_to = "docs"
 
-    copy_to_target_directory("static","docs")
-    generate_pages_recursively("content","template.html","docs",basepath)
+    copy_to_target_directory("static", copy_to)
+    generate_pages_recursively("content","template.html", copy_to, basepath)
     
 if __name__ == "__main__":
     main()
